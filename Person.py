9e50848e21a95c9db5882720778b5789eceb9ad3
Espen Sele <espesele@stud.ntnu.no>
2023-10-13 11:54:16 +0200

    Better showcased scenario

--- Person.py
+++ Person.py
@@ -8,7 +8,7 @@ class Person:
         self.e = returnE(self.phi)
         self.public_key = None
         self.private_key = None
-        self.message = "HELLO THIS IS THE MESSAGE"
+        self.message = ""
         self.encrypted = ""
 
     def generate_public_key(self):
@@ -20,12 +20,35 @@ class Person:
         return self.private_key
 
 alice = Person()
+bob = Person()
 alice.public_key = alice.generate_public_key()
 alice.private_key = alice.generate_private_key()
-print(alice.public_key)
-print(alice.private_key)
-encrypted = encrypt(alice.message, alice.public_key)
-alice.encrypted = encrypted
-decrypted = decrypt(alice.encrypted, alice.private_key)
+bob.public_key = bob.generate_public_key()
+bob.private_key = bob.generate_private_key()
+if alice.public_key is None:
+    print("alice.public_key is None")
+else:
+    alice.private_key = alice.generate_private_key()
+    print("A pub", alice.private_key)
+
+if alice.private_key is None:
+    print("alice.private_key is None")
+else:
+    alice.private_key = alice.generate_private_key()
+    print("A priv", alice.private_key)
+
+if bob.public_key is None:
+    print("bob.public_key is None")
+else:
+    print("B pub", bob.public_key)
+if bob.private_key is None:
+    print("bob.private_key is None")
+else:
+    print("B priv", bob.private_key)
+
+bob.messsage = "Hello, this is bob"
+alice.encrypted = encrypt(bob.message, alice.public_key)
 print(alice.encrypted)
-print(decrypted)
+#Alice receives the encrypted message from Bob
+bob.message = decrypt(alice.encrypted, alice.private_key)
+print(bob.message)
